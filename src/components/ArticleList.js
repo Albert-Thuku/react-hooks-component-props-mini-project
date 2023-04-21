6a353c9af822d37b2bfddfe557b4ef4e69bd9e84
Albert Thuku <athuku02@gmail.com>
2023-04-21 13:30:30 +0300

    Added a key to Article component

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -5,7 +5,7 @@ function ArticleList({post}){
     return (
         <main>
             {post.map((art)=>{
-                return <Article title={art.title} date={art.date} minute={art.minutes} preview={art.preview}/>
+                return <Article key={art.id} title={art.title} date={art.date} minute={art.minutes} preview={art.preview}/>
             })}
         </main>
     );
